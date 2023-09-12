4a3af1b27aa194435c91612a0d2efee33bf690c8
EC2 Default User <ec2-user@ip-172-31-13-13.ap-northeast-1.compute.internal>
2023-09-12 11:03:40 +0000

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,10 @@ Rails.application.routes.draw do
 
   resources :groups, except: [:destroy] do
     resource :group_users, only: [:create, :destroy]
+    get "new/mail" => "groups#new_mail"
+    get "send/mail" => "groups#send_mail"
    end
+
   resources :users, only: [:index,:show,:edit,:update] do
             member do
             get :follows, :followers
